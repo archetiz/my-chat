c6dd3822902af440cfe61d29efbf94dc5fb8cf0f
Kókai Kristóf <archetiz@sch.bme.hu>
2020-05-17 19:58:34 +0200

    Build

---
+++ build/precache-manifest.b7dad86522b648856bed69602b6aacdf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3e845e35d69874f27c51480a74b0069",
+    "url": "./index.html"
+  },
+  {
+    "revision": "caa5cabaae89e1e95462",
+    "url": "./static/css/main.179ebc87.chunk.css"
+  },
+  {
+    "revision": "f9f1c85ed2dfb059483d",
+    "url": "./static/js/2.db39bea0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.db39bea0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caa5cabaae89e1e95462",
+    "url": "./static/js/main.35e07afd.chunk.js"
+  },
+  {
+    "revision": "c999e1aafeacb3931724",
+    "url": "./static/js/runtime-main.9edb4eab.js"
+  }
+]);
